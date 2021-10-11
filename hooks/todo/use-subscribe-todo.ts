ea35522c6dfa936761c7543e3623401e658f5be1
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2021-10-11 12:45:00 +0900

    Deleted comment.

--- hooks/todo/use-subscribe-todo.ts
+++ hooks/todo/use-subscribe-todo.ts
@@ -4,27 +4,19 @@ import { onCreateTodo, onDeleteTodo } from '../../src/graphql/subscriptions';
 import { GraphQLResult } from '@aws-amplify/api';
 import { OnCreateTodoSubscription, Todo, OnDeleteTodoSubscription } from '../../src/API';
 import { Observable } from 'zen-observable-ts';
-import useSWR from 'swr';
 import { AWSAppSyncRealTimeProvider } from '@aws-amplify/pubsub';
-import { useSWRConfig, KeyedMutator } from 'swr';
+import { useSWRConfig } from 'swr';
 
 export const useSubscribeTodo = () => {
-  // export const useSubscribeTodo = (data: Todo[] | undefined, mutate: KeyedMutator<Todo[]>) => {
-  // export const useSubscribeTodo = (data: Todo[] | undefined) => {
-  // const { data, mutate } = useSWR<Todo[]>('listTodos', null, { fallbackData: [] });
-  // const { data, mutate } = useSWR<Todo[]>('listTodos', null);
   const { mutate } = useSWRConfig();
-  // console.log('subscribe before data:', data);
+
   useEffect(() => {
     const pubSubClient: Observable<object> = API.graphql(graphqlOperation(onCreateTodo));
     const subscription = pubSubClient.subscribe({
       next: (payload: { provider: AWSAppSyncRealTimeProvider; value: GraphQLResult<OnCreateTodoSubscription> }) => {
-        // console.log('Create useSubscribeTodo data:', data);
-        // if (data && payload.value.data && payload.value.data.onCreateTodo) {
         if (payload.value.data && payload.value.data.onCreateTodo) {
           console.log('Create Subscribe data: ', payload.value.data.onCreateTodo);
           mutate('listTodos');
-          // mutate([...data, payload.value.data.onCreateTodo], false);
         }
       },
       error: (error) => console.error('Create subscribing error:', error),
@@ -38,16 +30,10 @@ export const useSubscribeTodo = () => {
     const pubSubClient: Observable<object> = API.graphql(graphqlOperation(onDeleteTodo));
     const subscription = pubSubClient.subscribe({
       next: (payload: { provider: AWSAppSyncRealTimeProvider; value: GraphQLResult<OnDeleteTodoSubscription> }) => {
-        // console.log('Delete useSubscribeTodo data:', data);
-        // if (data && payload.value.data && payload.value.data.onDeleteTodo) {
         if (payload.value.data && payload.value.data.onDeleteTodo) {
           console.log('Delete Subscribe data: ', payload.value.data.onDeleteTodo);
           const deleteId = payload.value.data.onDeleteTodo.id;
           mutate('listTodos');
-          // mutate(
-          //   data.filter((item) => item.id === deleteId),
-          //   false
-          // );
         }
       },
       error: (error) => console.error('Delete subscribing error:', error),
